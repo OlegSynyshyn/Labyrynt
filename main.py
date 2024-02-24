108257a677756505345987ad5d6ec524eabe3141
OlegSynyshyn <145208359+OlegSynyshyn@users.noreply.github.com>
2024-02-24 17:20:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,21 +13,48 @@ class GameSprite():
         self.move_left=False
         self.move_up=False
         self.move_down=False
-    def reset(self):
-        if self.move_left == True:
-            self.rect.x -= self.speed
-        if self.move_right == True:
-            self.rect.x += self.speed
-        if self.move_up == True:
-            self.rect.y -= self.speed
-        if self.move_down == True:
-            self.rect.y += self.speed
+        self.direction="right"
 
-
-
-
-
+    def reset(self): 
         window.blit(self.img, (self.rect.x, self.rect.y))
+
+class Hero(GameSprite):
+   
+    def move (self):
+        keys = key.get_pressed()
+        if keys[K_d]:
+            if self.rect.x < 640:
+                self.rect.x += self.speed
+        if keys[K_a]:
+            if self.rect.x > 5:
+                self.rect.x -= self.speed
+        if keys [K_w]:
+            if self.rect.y > 5:
+                self.rect.y -= self.speed
+        if keys[K_s]:
+            if self.rect.y < 440:
+                self.rect.y += self.speed
+        
+class Enemy(GameSprite):
+    def move(self, start, end):
+    
+        if self.direction == "right":
+            self.rect.x+=5
+
+        if self.direction == "left":
+            self.rect.x -= 5
+
+        if self.rect.x >= end:
+            self.direction = "left"
+        if self.rect.x <= start:
+            self.direction = "right"
+
+
+
+
+
+
+        
         
 
 
@@ -43,8 +70,9 @@ window.blit(background, (0, 0))
 
 clock = time.Clock()
 speed = 10
-cyborg = GameSprite("cyborg.png",10, 50, 120)
-hero = GameSprite("hero.png",10, 50, 50)
+cyborg = Enemy("cyborg.png",10, 50, 120)
+cyborg1 = Enemy("cyborg.png",10, 50, 120)
+hero1 = Hero("hero.png",10, 50, 50)
 
 x1 = 50
 y1 = 100
@@ -55,56 +83,68 @@ game = True
 
 while game:
     window.blit(background, (0, 0))
-    hero.reset()
+    hero1.reset()
     cyborg.reset()
-
+    cyborg1.reset()
+    hero1.move()
+    cyborg.move(300, 600)
+    cyborg1.move(150, 400)
     window.blit(treasure, (350,350))
 
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if e.type == KEYDOWN:
-            if e.key == K_1:
-                kick.play()
-            if e.key == K_a:
-                cyborg.move_left = True
-            if e.key == K_d:
-                cyborg.move_right = True
-            if e.key == K_w:
-                cyborg.move_up = True   
-            if e.key == K_s:
-                cyborg.move_down = True    
 
 
-            if e.key == K_LEFT:
-                hero.move_left = True
-            if e.key == K_RIGHT:
-                hero.move_right = True
-            if e.key == K_UP:
-                hero.move_up = True   
-            if e.key == K_DOWN:
-                hero.move_down = True 
-
-        if e.type == KEYUP:
-
-            if e.key == K_a:
-                cyborg.move_left = False
-            if e.key == K_d:
-                cyborg.move_right = False
-            if e.key == K_w:
-                cyborg.move_up = False   
-            if e.key == K_s:
-                cyborg.move_down = False  
 
 
-            if e.key == K_LEFT:
-                hero.move_left = False
-            if e.key == K_RIGHT:
-                hero.move_right = False
-            if e.key == K_UP:
-                hero.move_up = False   
-            if e.key == K_DOWN:
-                hero.move_down = False 
+
+
+
+
+
+        # if e.type == KEYDOWN:
+        #     if e.key == K_1:
+        #         kick.play()
+        #     if e.key == K_a:
+        #         cyborg.move_left = True
+        #     if e.key == K_d:
+        #         cyborg.move_right = True
+        #     if e.key == K_w:
+        #         cyborg.move_up = True   
+        #     if e.key == K_s:
+        #         cyborg.move_down = True    
+
+
+        #     if e.key == K_LEFT:
+        #         hero.move_left = True
+        #     if e.key == K_RIGHT:
+        #         hero.move_right = True
+        #     if e.key == K_UP:
+        #         hero.move_up = True   
+        #     if e.key == K_DOWN:
+        #         hero.move_down = True 
+
+        # if e.type == KEYUP:
+
+        #     if e.key == K_a:
+        #         cyborg.move_left = False
+        #     if e.key == K_d:
+        #         cyborg.move_right = False
+        #     if e.key == K_w:
+        #         cyborg.move_up = False   
+        #     if e.key == K_s:
+        #         cyborg.move_down = False  
+
+
+        #     if e.key == K_LEFT:
+        #         hero.move_left = False
+        #     if e.key == K_RIGHT:
+        #         hero.move_right = False
+        #     if e.key == K_UP:
+        #         hero.move_up = False   
+        #     if e.key == K_DOWN:
+        #         hero.move_down = False 
 
 
     # keys_pressed = key.get_pressed()
